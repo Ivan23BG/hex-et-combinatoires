985f86b89c983fee876f39917f7865652ac89d87
Ivan23BG <ilejeune@outlook.com>
2023-12-17 11:09:23 +0100

    Updated the visuals on board display and increased readability of code and functions

--- src/main/hexgame/board/hexboard.py
+++ src/main/hexgame/board/hexboard.py
@@ -1,79 +1,176 @@
 # HexGame/board/hex_board.py
+class InvalidPositionError(Exception):
+    pass
+
+
+class InvalidPlayerError(Exception):
+    pass
+
+
+class PositionOccupiedError(Exception):
+    pass
+
+
 class HexBoard:
     def __init__(self, size):
-        # Initialisation du plateau de jeu
+        """
+        Initialize the class with the given size.
+    
+        Args:
+            size (int): The size of the board.
+        """
+        if size <= 0:
+            raise ValueError("Size must be a positive integer")
         self.size = size
-        self.board = [[0] * size for _ in range(size)]
+        self.board = None
+        self.initialize_board()
+
+    def initialize_board(self):
+        """
+        Initialize the board with zeros.
+        """
+        self.board = [[0] * self.size for _ in range(self.size)]
 
     def place_piece(self, player, position):
-        # Logique pour placer une pièce sur le plateau
+        """
+        Place a piece on the board.
+        
+        Args:
+            player (int): The player value (1 or 2).
+            position (tuple): The position on the board.
+        """
+        # Place a piece on the board
         row, col = position
+
+        # Check if the position is valid
+        if not self.is_valid_position(row, col):
+            raise InvalidPositionError("Invalid position")
+
+        # Check if the player value is valid
+        if player not in [1, 2]:
+            raise InvalidPlayerError("Invalid player value. Player must be 1 or 2.")
+
+        # Check if the position is already occupied
+        if self.is_position_occupied(position):
+            raise PositionOccupiedError("Position already occupied")
+
         self.board[row][col] = player
 
+    def is_valid_position(self, row, col):
+        """
+        Check if the position is within the bounds of the board.
+
+        Args:
+            row (int): The row index.
+            col (int): The column index.
+
+        Returns:
+            bool: True if the position is valid, False otherwise.
+        """
+        return 0 <= row < self.size and 0 <= col < self.size
+
+    def is_position_occupied(self, position):
+        """
+        Check if the position is already occupied.
+
+        Args:
+            position (tuple): The position on the board.
+
+        Returns:
+            bool: True if the position is occupied, False otherwise.
+        """
+        row, col = position
+        return self.board[row][col] != 0
+
     def check_winner(self):
-        # Vérifie s'il y a un gagnant
-        # On considère deux côtés opposés comme les côtés supérieur et inférieur pour le joueur 1,
-        # et les côtés gauche et droit pour le joueur 2
-        player_1_side = [(i, 0) for i in range(self.size)]  # Côté supérieur
-        player_2_side = [(0, j) for j in range(self.size)]  # Côté gauche
+        """
+        Check if there is a winner.
 
-        # On vérifie la connexion du côté supérieur au côté inférieur pour le joueur 1
-        if any(self._dfs_connected(player_1_side, (i, 0), set()) for i in range(self.size)):
-            return 1  # Le joueur 1 a gagné
+        Returns:
+            int: The player value (1 or 2) if there is a winner, None otherwise.
+        """
+        for i in range(self.size):
+            if self.board[i][0] == 1:  # left side
+                if self.check_winner_player(1, (i, 0)):
+                    return 1
+            if self.board[0][i] == 2:  # top side
+                if self.check_winner_player(2, (0, i)):
+                    return 2
+        return None
 
-        # On vérifie la connexion du côté gauche au côté droit pour le joueur 2
-        if any(self._dfs_connected(player_2_side, (0, j), set()) for j in range(self.size)):
-            return 2  # Le joueur 2 a gagné
+    def check_winner_player(self, player, position, visited=None):
+        """
+        Check if the player has won.
 
-        # Aucun gagnant n'a été trouvé
-        return 0
+        Args:
+            player (int): The player value (1 or 2).
+            position (tuple): The position on the board.
+            visited (list): The list of visited positions.
 
-    def _dfs_connected(self, side, current, visited):
-        # Fonction DFS pour vérifier la connexion entre deux côtés
-        i, j = current
-        visited.add(current)
-
-        if current in side:
+        Returns:
+            bool: True if the player has won, False otherwise.
+        """
+        if visited is None:
+            visited = []
+        row, col = position
+        visited.append(position)
+        if col == self.size - 1 and player == 1:  # we're on the right side
+            print("Winning path:", visited)
+            return True
+        if row == self.size - 1 and player == 2:  # we're on the bottom side
+            print("Winning path:", visited)
             return True
 
-        neighbors = self._get_neighbors(i, j)
-        connected = any(neighbor not in visited and self.board[neighbor[0]][neighbor[1]] == self.board[i][j] and
-                        self._dfs_connected(side, neighbor, visited)
-                        for neighbor in neighbors)
-        return connected
-
-    def _get_neighbors(self, i, j):
-        # Renvoie les voisins valides d'une cellule
-        neighbors = [(i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1), (i - 1, j + 1), (i + 1, j - 1)]
-        return [(ni, nj) for ni, nj in neighbors if 0 <= ni < self.size and 0 <= nj < self.size]
+        if row > 0:
+            if (row - 1, col) not in visited:
+                if self.board[row - 1][col] == player:
+                    return self.check_winner_player(player, (row - 1, col), visited)  # top left
+        if row > 0 and col < self.size - 1:
+            if (row - 1, col + 1) not in visited:
+                if self.board[row - 1][col + 1] == player:
+                    return self.check_winner_player(player, (row - 1, col + 1), visited)  # top right
+        if row < self.size - 1 and col > 0:
+            if (row + 1, col - 1) not in visited:
+                if self.board[row + 1][col - 1] == player:
+                    return self.check_winner_player(player, (row + 1, col - 1), visited)  # bottom left
+        if row < self.size - 1:
+            if (row + 1, col) not in visited:
+                if self.board[row + 1][col] == player:
+                    return self.check_winner_player(player, (row + 1, col), visited)  # bottom right
+        if col < self.size - 1:
+            if (row, col + 1) not in visited:
+                if self.board[row][col + 1] == player:
+                    return self.check_winner_player(player, (row, col + 1), visited)  # right
+        if col > 0:
+            if (row, col - 1) not in visited:
+                if self.board[row][col - 1] == player:
+                    return self.check_winner_player(player, (row, col - 1), visited)  # left
+        return False
 
     def display_board(self):
-        # Display the board
+        """
+        Display the board.
+        """
+        # add an offset to the board to make it look like a hexagon
+
+        board_string = ""
+        separator = " | "
+        symbol_1 = "X"
+        symbol_2 = "O"
+        horizontal_line = " ---" * self.size
+        offset = ""
         for i in range(self.size):
-            print(" ---" * self.size)
-            print("| ", end="")
+            # Add offset to the line
+            offset = " " * (i * 2)
+            board_string += offset + horizontal_line + "\n"
+            board_string += offset + "| "
             for j in range(self.size):
                 if self.board[i][j] == 1:
-                    print("X", end=" | ")
+                    board_string += symbol_1 + separator
                 elif self.board[i][j] == 2:
-                    print("O", end=" | ")
+                    board_string += symbol_2 + separator
                 else:
-                    print(" ", end=" | ")
-            print()
-        print(" ---" * self.size)
-# Exemple d'utilisation
-size = 4
-game_board = HexBoard(size)
-game_board.display_board()
-game_board.place_piece(1, (0, 0))
-game_board.place_piece(1, (1, 1))
-game_board.place_piece(1, (2, 2))
-game_board.place_piece(1, (3, 3))
-game_board.display_board()
-game_board.place_piece(2, (0, 1))
-game_board.place_piece(2, (1, 2))
-game_board.place_piece(2, (2, 3))
-game_board.place_piece(2, (3, 0))
-game_board.display_board()
-winner = game_board.check_winner()
-print("Le joueur", winner, "a gagné.")
+                    board_string += "*" + separator
+            board_string += "\n"
+        board_string += offset + horizontal_line
+        print(board_string)
--- src/main/main.py
+++ src/main/main.py
@@ -1,8 +1,20 @@
 # main.py
-from src.main.hexgame.board import hexboard
+from src.main.hexgame.board.hexboard import HexBoard
 
 
-if __name__ == "__main__":
-    size = 7
-    game_board = HexBoard(size)
-    print(game_board.board)  # Vérifiez que le plateau est correctement initialisé
+# Example 1
+size = 3
+game_board = HexBoard(size)
+game_board.place_piece(1, (2, 0))
+game_board.place_piece(2, (0, 1))
+game_board.place_piece(1, (0, 2))
+game_board.place_piece(2, (1, 1))
+game_board.place_piece(1, (1, 2))
+game_board.place_piece(2, (2, 2))
+game_board.place_piece(1, (2, 1))
+game_board.display_board()
+winner = game_board.check_winner()
+if winner:
+    print("Player", winner, "won!")
+else:
+    print("No one has won yet")
