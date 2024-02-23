0c9b01fa6b1ca5724b7be689dbc5e5715818f597
Ivan23BG <ilejeune@outlook.com>
2024-02-23 13:05:54 +0100

    refactoring functions and files

--- src/main/hexgame/app.py
+++ src/main/hexgame/app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 # create a global variable to store the game board
 game_board = None
 current_player = 1
-size = 0
+size = 5
 size_px = (size+size)*45
 
 
@@ -21,15 +21,33 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/init_board', methods=['POST'])
+# handle the load game request
+@app.route('/load_game', methods=['POST'])
 def init_board():
-    global game_board, current_player, size_px
-    size = int(request.form['size'])
-    size_px = (size+size)*45
-    game_board = HexBoard(size)
+    global game_board, current_player, size_px, size
+    size_px = (size+size)*45 # update the size_px used in the play.html
+    game_board = HexBoard(size) # Create a new game board
+    print("Game board initialized")
     game_board.display_board()
     current_player = 1  # Set player 1 as the starting player
+    print("Current player: ", current_player)
     return render_template('play.html', size=size, size_px=size_px, current_player=current_player)
+
+
+# handle the reload game request
+@app.route('/reload_game', methods=['POST'])
+def reload_board():
+    global game_board, current_player, size_px, size
+    data = request.get_json()
+    size = data['size']
+    size_px = (size+size)*45 # update the size_px used in the play.html
+    game_board = HexBoard(size) # Create a new game board
+    print("Game board reinitialized")
+    game_board.display_board()
+    current_player = 1  # Set player 1 as the starting player
+    print("Current player: ", current_player)
+    return jsonify({'size': size, 'size_px': size_px, 'current_player': current_player})
+
 
 
 # handle the place piece request
@@ -49,16 +67,17 @@ def place_piece():
             game_board.place_piece(current_player, (row, col))
             current_player = 1 if current_player == 2 else 2
             game_board.display_board()
+            print("Current player: ", current_player)
             # check if the current player won
             winner = game_board.check_winner()
             if winner:
-                return jsonify({'winner': current_player})
+                return jsonify({'winner': current_player, 'game_over': True, 'current_player': current_player})
     except Exception as e:
         # Handle the exception here
         error_message = str(e)  # Get the error message
         return jsonify({'error': error_message}), 400
 
-    return jsonify({'result': 'Success'})
+    return jsonify({'result': 'Success', 'current_player': current_player})
 
 
 # necessary for to handle the error play return
@@ -69,10 +88,3 @@ def get_place_piece():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-@app.route('/reset', methods=['POST'])
-def reset():
-    global game_board
-    game_board = HexBoard(size)
-    return jsonify({'message': 'Board reset'})
