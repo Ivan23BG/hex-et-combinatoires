82bd9c69c3200087eb27941e5901ebdc3e830bc6
gubace <bonettim060@gmail.com>
2024-03-08 21:48:59 +0100

    avancés sur la page d'acceuil

---
+++ src/main/hexgame/static/js/index.js
@@ -0,0 +1,16 @@
+function parallax_height() {
+    var scroll_top = $(this).scrollTop();
+    var section_top = $(".container").offset().top;
+    var header_height = $(".header-section").outerHeight();
+    $(".container").css({ "margin-top": header_height });
+    $(".header").css({ height: header_height - scroll_top });
+}
+
+parallax_height();
+$(window).scroll(function () {
+    parallax_height();
+});
+
+$(window).resize(function () {
+    parallax_height();
+});
