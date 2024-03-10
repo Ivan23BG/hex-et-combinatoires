7ea8ac8f8c875b5934189871cf32e1231a1ac24c
gubace <bonettim060@gmail.com>
2024-03-10 20:43:11 +0100

    ajouts boutons choix taille

---
+++ src/main/hexgame/static/js/home_hex.js
@@ -0,0 +1,28 @@
+var size = 11; // Valeur initiale de la variable 'size'
+var selectedDiv = null;
+
+function changeSize(newSize) {
+    size = newSize;
+}
+
+function handleClick(newSize, divNumber) {
+    changeSize(newSize);
+    selectDiv(divNumber);
+}
+
+
+
+function selectDiv(divNumber) {
+    if (selectedDiv !== null) {
+        // Désélectionner la div précédemment sélectionnée
+        selectedDiv.classList.remove('selected');
+    }
+    // Sélectionner la nouvelle div
+    selectedDiv = document.getElementById('div' + divNumber);
+    selectedDiv.classList.add('selected');
+}
+
+function submitForm() {
+    document.getElementById("size_input").value = size;
+    document.forms[0].submit();
+}
