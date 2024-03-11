a8e7bb1f3344d1d4316e763113f74c791cdfbf6d
gubace <bonettim060@gmail.com>
2024-03-11 11:20:54 +0100

    fonctionne

--- src/main/hexgame/app.py
+++ src/main/hexgame/app.py
@@ -29,8 +29,7 @@ def home_hex():
 @app.route('/load_game', methods=['POST'])
 def init_board():
     global game_board, current_player, size_px, size
-    data = request.get_json()
-    size = data['size']
+    size = int(request.form['size'])
     size_px = 120 + (44 * size)  # update the size_px used in the play.html
     game_board = HexBoard(size)  # Create a new game board
     game_board.display_board()  # Display the game board in the console
