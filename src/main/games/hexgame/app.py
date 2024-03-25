a453d357a83ee04d30cc9a7168ac3ecf88c9ed3a
gubace <bonettim060@gmail.com>
2024-03-25 15:47:29 +0100

    ca marche po

--- src/main/games/hexgame/app.py
+++ src/main/games/hexgame/app.py
@@ -84,6 +84,62 @@ def hex_place_piece():
 
     return jsonify({'result': 'Success', 'current_player': current_player})
 
+
+
+
+@app.route('/hex_place_piece_ia', methods=['POST']) # Place a piece on the board
+def hex_place_piece_ia():
+    global game_board, current_player
+    
+    data = request.get_json()
+    hexid = data['hexid']
+    current_player = data['current_player']
+    
+    # Remove the "hex" prefix and split into row and column
+    row, col = map(int, hexid[3:].split('-'))
+
+    # change the current player
+    try:
+        if game_board is not None:
+            game_board.place_piece(1, (row, col)) # Try to place the piece
+            print('1')
+            game_board.display_board() # Display the game board in the console
+            
+            # check if the current player won
+            winner = game_board.check_winner()
+            if winner:
+                short_path = game_board.shortest_path(current_player)
+                print(f"Shortest path for player {current_player}: {short_path}")
+                hexid = [f"hex{i[0]}-{i[1]}" for i in short_path]
+                return jsonify({'winner': current_player, 'game_over': True, 'current_player': current_player,'hexid':hexid})
+            
+            #IA's turn
+            # make a move using minimax algorithm and get_best_move method
+            move = game_board.get_best_move(3,2)    
+            game_board.place_piece(2, move)
+            print('2')
+            game_board.display_board()
+
+            # check if PC won
+            winner = game_board.check_winner()
+            if winner:
+                short_path = game_board.shortest_path(current_player)
+                print(f"Shortest path for player {current_player}: {short_path}")
+                hexid = [f"hex{i[0]}-{i[1]}" for i in short_path]
+                return jsonify({'winner': current_player, 'game_over': True, 'current_player': current_player,'hexid':hexid})
+                
+            current_player = 1
+            
+    except Exception as e:
+        # Handle the exception here
+        error_message = str(e)  # Get the error message
+        return jsonify({'error': error_message}), 400
+
+    return jsonify({'result': 'Success', 'current_player': current_player})
+
+
+
+
 @app.route('/undo_move', methods=['POST']) # Place a piece on the board
 def undo_move():
     global game_board, current_player
