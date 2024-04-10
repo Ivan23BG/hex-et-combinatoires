7094dbe1ae34ddf42620c11da44325ce7eebee47
RaphBi <2raphael.bigey@gmail.com>
2024-04-10 20:57:44 +0200

    ça marche pas

--- src/main/game_logic/hexgame/board/hexboard.py
+++ src/main/game_logic/hexgame/board/hexboard.py
@@ -682,3 +682,12 @@ class HexBoard:
     def get_best_move(self, depth, player):
         _ , best_move = self.minimax(depth, player, float('-inf'), float('inf'))
         return best_move
+
+    def random_move(self):
+        Trouve = False
+        while(not Trouve):
+            x = random.randint(0,self.size)
+            y = random.randint(0,self.size)
+            if not(self.is_position_occupied((x,y))):
+                Trouve = True
+                return (x,y)
