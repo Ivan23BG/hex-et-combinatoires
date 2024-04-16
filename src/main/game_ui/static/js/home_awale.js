e45ad0e2911203704a628796a3b0146507d9f49c
gubace <bonettim060@gmail.com>
2024-04-16 23:57:06 +0200

    setup nouveaux fichiers

--- src/main/game_ui/static/js/home_awale.js
+++ src/main/game_ui/static/js/home_awale.js
@@ -1,3 +1,6 @@
+var player = 1;
+
+
 function back() {
     window.location.href = '/';
 }
@@ -7,8 +10,25 @@ function start1v1() {
     window.location.href = '/game_awale';
 }
 
-function submitForm() {
-    document.forms[0].submit();
+function submitForm(type) {
+    if (type == 1){
+    document.forms[0].submit();}
+    if (type == 2){
+        document.getElementById('player').value = player;
+        document.querySelector('form[action="/game_awaleia"]').submit();
+    }
 }
 
-//oui
+
+
+function player_red(){
+    document.getElementById("button_red").classList.add("selected_2")
+    document.getElementById("button_blue").classList.remove("selected_2")
+    player = 1;
+}
+
+function player_blue(){
+    document.getElementById("button_red").classList.remove("selected_2")
+    document.getElementById("button_blue").classList.add("selected_2")
+    player = 2;
+}
