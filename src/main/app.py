0f792788d11cacc4ac5392614feac9d4fcac6c79
Ivan23BG <ilejeune@outlook.com>
2024-05-13 09:53:53 +0200

    Fix Weaknesses CWE215 and CWE489

--- src/main/app.py
+++ src/main/app.py
@@ -345,4 +345,4 @@ def undo_move_awale():
 
 # --------------------------------- Run the app --------------------------------- #
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
